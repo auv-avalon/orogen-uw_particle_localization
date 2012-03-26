a1ebc7efa7d3ecfc76e0a5cae3ecfc36afba09e0
Chris Mueller <christoph.mueller@dfki.de>
2012-03-27 01:11:14 +0200

    add old dynamic update for speed samples and use its covariance for adding particle noise.

--- tasks/ParticleLocalization.cpp
+++ tasks/ParticleLocalization.cpp
@@ -1,2 +1,67 @@
 #include "ParticleLocalization.hpp"
+#include <machine_learning/RandomNumbers.hpp>
 
+using namespace machine_learning;
+
+namespace uw_localization {
+
+double ParticleLocalization::dynamic(PoseParticle& X, const base::samples::RigidBodyState& U)
+{
+    MultiNormalRandom<3> Noise = Random::multi_gaussian(U.velocity, U.cov_velocity);
+    PoseParticle Xt;
+    
+    base::Vector3d v_noisy = Noise();
+    base::Vector3d v_avg = (X.velocity + v_noisy) / 2.0;
+
+    if( !X.timestamp.isNull() ) {
+        double dt = (U.time - X.timestamp).toSeconds();
+
+        Xt.position = X.position + vehicle_orientation * (v_avg * dt);
+    }
+
+    Xt.velocity = v_noisy;
+    Xt.timestamp = U.time;
+    Xt.confidence = X.confidence;
+ 
+    return 0;
+}
+
+double ParticleLocalization::perception(PoseParticle& state, const base::samples::LaserScan& scan, const StochasticMap& map)
+{
+    return 0;
+}
+  
+base::Vector3d ParticleLocalization::position(const PoseParticle& state) const
+{
+    return state.position;
+}
+
+
+double ParticleLocalization::yaw(const PoseParticle& state) const
+{
+    return 0;
+}
+
+
+double ParticleLocalization::get_weight(const PoseParticle& state) const
+{
+    return state.confidence;
+}
+
+
+
+void ParticleLocalization::set_weight(PoseParticle& state, double value)
+{
+    state.confidence = value;
+}
+
+
+  
+base::samples::RigidBodyState ParticleLocalization::estimate() const
+{
+    base::samples::RigidBodyState state;
+
+    return state;
+}
+
+}
--- tasks/ParticleLocalization.hpp
+++ tasks/ParticleLocalization.hpp
@@ -12,14 +12,35 @@
 #include <base/samples/rigid_body_state.h>
 #include <base/samples/laser_scan.h>
 #include <uw_localization/filters/particle_filter.hpp>
+#include <uw_localization/maps/stochastic_map.hpp>
 
 namespace uw_localization {
 
 struct PoseParticle {
-    base::Position position;
-    base::Vector3d velocity;
-    base::Time timestamp;
-    double confidence;
+  base::Position position;
+  base::Vector3d velocity;
+  base::Time timestamp;
+  double confidence;
+};
+
+
+class ParticleLocalization : public ParticleFilter<PoseParticle, StochasticMap>,
+  public Dynamic<PoseParticle, base::samples::RigidBodyState>,
+  public Perception<PoseParticle, base::samples::LaserScan, StochasticMap>
+{
+public:
+  virtual double dynamic(PoseParticle& state, const base::samples::RigidBodyState& speed);
+  virtual double perception(PoseParticle& state, const base::samples::LaserScan& scan, const StochasticMap& map);
+  
+  virtual base::Vector3d position(const PoseParticle& state) const;
+  virtual double yaw(const PoseParticle& state) const;
+  virtual double get_weight(const PoseParticle& state) const;
+  virtual void set_weight(PoseParticle& state, double value);
+  
+  virtual base::samples::RigidBodyState estimate() const;
+
+private:
+  base::Orientation vehicle_orientation;
 };
 
 
